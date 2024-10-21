a9322c526629d1e6c3d94ed1a90658e72b41733f
Toddthegod1 <klingert@dickinson.edu>
2024-10-21 13:43:42 -0400

    Added tests for the dropdown and fixed the problem I had from API2 yay! lets gogit add .git add .

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -10,12 +10,29 @@ describe("Test the harvest report default values", () => {
     })
 
     it("Check the start date", () => {
-        cy.get("[data-cy=start-date")
+        cy.get("[data-cy=start-date]")
         .should("have.value", "2020-05-05")
     })
 
     it("Check the end date", () => {
-        cy.get("[data-cy=end-date")
+        cy.get("[data-cy=end-date]")
         .should("have.value", "2020-05-15")
+    })
+
+    it("Check the crop dropdown", () => {
+        cy.get("[data-cy=crop-dropdown]")
+        .should("exist")
+
+        cy.get("[data-cy=crop-dropdown]").children().eq(0)
+        .should("have.text", " ARUGULA ")
+
+        cy.get("[data-cy=crop-dropdown]").children().eq(4)
+        .should("have.text", " BEAN-FAVA ")
+
+        cy.get("[data-cy=crop-dropdown]").children().last()
+        .should("have.text", " ZUCCHINI ")
+
+        cy.get("[data-cy=crop-dropdown]").children()
+        .should("have.length", 111)
     })
 })
