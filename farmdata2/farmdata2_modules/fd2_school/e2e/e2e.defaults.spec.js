75ee20d476e4ab915dd617b9bcc47593d2bd5efd
Toddthegod1 <klingert@dickinson.edu>
2024-10-21 12:38:26 -0400

    Added start and end date tests

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -8,4 +8,14 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy=page-header]")
         .should("have.text", "Harvest Report")
     })
+
+    it("Check the start date", () => {
+        cy.get("[data-cy=start-date")
+        .should("have.value", "2020-05-05")
+    })
+
+    it("Check the end date", () => {
+        cy.get("[data-cy=end-date")
+        .should("have.value", "2020-05-15")
+    })
 })
